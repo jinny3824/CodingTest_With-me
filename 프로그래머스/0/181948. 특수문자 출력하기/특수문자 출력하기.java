30fe365a4de953d0933e276368eb43099e6a20cf
promin <31163623+jinny3824@users.noreply.github.com>
2025-06-27 13:18:50 +0900

    [level 0] Title: 특수문자 출력하기, Time: 131.01 ms, Memory: 68.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181948. 특수문자 출력하기/특수문자 출력하기.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        
+        //백슬래시랑 따옴표 쌍따옴표는 백슬래쉬를 앞에 붙인 상태로 출력
+        
+        System.out.print("!@#$%^&*(\\'\"<>?:;");
+        
+    }
+}
